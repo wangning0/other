5abad34839b04a6811bbb9985422fa6727169be4
wangning0 <172234437@qq.com>
2016-04-09 19:30:27 +0800

    由gulp-connect 改为webpack-dev-server

--- react-gulp-hanging/gulpfile.js
+++ react-gulp-hanging/gulpfile.js
@@ -1,62 +1,17 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
-var path = require('path');
-var shell = require('gulp-shell');
-var watch = require('gulp-watch');
 var webpack = require('gulp-webpack');
-var uglify = require('gulp-uglify');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require("html-webpack-plugin");
 var rimraf = require('gulp-rimraf');
 var runSequence = require('run-sequence');
-var webpackConfig = {
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: "[name].[hash:8].js",
-		publicPath: '/'
-	},
-	resolve: {
-		extension: ['', '.jsx', '.js', '.json'],
-		alias: {}
-	},
-	'display-error-details': true,
-	module: {
-		noParse: [],
-		loaders: [{
-			test: /\.js[x]?$/,
-			loaders: ['react-hot', 'babel'],
-			exclude: path.resolve(__dirname, 'node_modules')
-		}, {
-			test: /\.css/,
-			loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-		}, {
-			test: /\.less/,
-			loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
-		}, {
-			test: /\.(png|jpg)$/,
-			loader: 'url?limit=8192'
-		}, {
-			test: /\.(woff|woff2|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
-			loader: "url?limit=10000"
-		}]
-	},
-	plugins: [
-		new ExtractTextPlugin("main.[hash:8].css", {
-			allChunks: true,
-			disable: false
-		}),
-		new HtmlWebpackPlugin({
-			title: 'your app title',
-			template: './app/index.html',
-		})
-	]
-}
+var WebpackConfig = require('./webpack.config.js');
+var Webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var gutil = require('gulp-util');
+
 gulp.task('pro_build', function() {
 	return gulp.src('./app/index.js')
-		.pipe(webpack(webpackConfig))
+		.pipe(webpack(WebpackConfig))
 		.pipe(gulp.dest('./build/'))
 });
-
 gulp.task('clean', function() {
 	gulp.src('./build', {
 			read: false
@@ -68,29 +23,19 @@ gulp.task('clean', function() {
 		})
 		.pipe(rimraf());
 });
-gulp.task('dev_build', function() {
-	return gulp.src('./app/index.js')
-		.pipe(webpack(webpackConfig))
-		.pipe(gulp.dest('./server/build/'))
-})
-gulp.task('connect', function() {
-	connect.server({
-		root: 'server/build',
-		livereload: true
+gulp.task('dev_build', function(cb) {
+	new WebpackDevServer(Webpack(WebpackConfig), {
+		contentBase: __dirname + './app/',
+		hot: true,
+		historyApiFallback: true,
+	}).listen(8080, 'localhost', function(err) {
+		if (err) throw new gutil.PluginError('webpack-dev-server', err);
+		gutil.log('[webpack-dev-server]', 'http://example.com:8080');
 	});
 });
-gulp.task('reload', function() {
-	gulp.src('./server/build/**/*')
-		.pipe(connect.reload());
-})
-gulp.task('watch', function() {
-	watch(['./app/**/*'], function() {
-		runSequence('clean', 'dev_build', 'reload')
-	})
-})
 gulp.task('pro', function() {
 	runSequence('clean', 'pro_build');
 })
 gulp.task('dev', function() {
-	runSequence('clean', 'dev_build', 'connect', 'watch');
+	runSequence('clean', 'dev_build');
 });
